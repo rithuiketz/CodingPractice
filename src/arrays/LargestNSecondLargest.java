aa4dbc6c22bd6fc83a269c3fe37ec506a69a30a7
Rithuik Yerrabrolu <rithuik.jsp@gmail.com>
2019-12-29 15:08:52 +0530

    Added the code

--- src/arrays/LargestNSecondLargest.java
+++ src/arrays/LargestNSecondLargest.java
@@ -2,6 +2,8 @@ package arrays;
 
 import static org.junit.Assert.*;
 
+import java.util.Arrays;
+
 import org.junit.Test;
 
 public class LargestNSecondLargest {
@@ -12,13 +14,32 @@ public class LargestNSecondLargest {
 		int[] testArr = {12, 35, 1, 10, 34, 1};
 		int[] expectedArr = {35,34};
 		testArr = findFirstNSecondLargest(testArr);
+		System.out.println(Arrays.toString(testArr));
+		assertArrayEquals(expectedArr, testArr);
 		
 		
 	}
 
-	private int[] findFirstNSecondLargest(int[] testArr) {
-		// TODO Auto-generated method stub
-		return null;
+	private int[] findFirstNSecondLargest(int[] testArr) 
+	{
+		int first=Integer.MIN_VALUE,second=Integer.MIN_VALUE;
+		for(int i=1;i<testArr.length;i++)
+		{
+			if(testArr[i] >  first)
+			{
+				second = first;
+				first=testArr[i];
+			}
+			else if(first > testArr[i] && testArr[i] > second)
+			{
+				second =  testArr[i];
+			}
+			
+			
+			
+		}
+		int[] arr= {first,second};
+		return arr;
 	}
 
 }
