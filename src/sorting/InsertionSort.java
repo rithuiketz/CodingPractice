8cafa4ed640b055c6b27608cefc8826620e79331
Rithuik Yerrabrolu <rithuik.jsp@gmail.com>
2020-01-02 11:48:22 +0530

    InsertionSort

---
+++ src/sorting/InsertionSort.java
@@ -0,0 +1,34 @@
+package sorting;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class InsertionSort {
+
+	@Test
+	public void test() {
+		int[] testArr = { 64, 25, 12, 22, 11 };
+		int[] expectedArr = { 11, 12, 22, 25, 64 };
+		sort(testArr);
+		assertArrayEquals(expectedArr, testArr);
+	}
+
+	private void sort(int[] testArr) 
+	{
+		int j,key,i,length=testArr.length;
+		for(i=0;i<length;i++)
+		{
+			j=i-1;
+			key=testArr[i];
+			while(j>=0 && testArr[j] > key)
+			{
+				testArr[j+1]=testArr[j];
+				--j;
+			}
+			testArr[j+1] = key;
+		}
+		
+	}
+
+}
