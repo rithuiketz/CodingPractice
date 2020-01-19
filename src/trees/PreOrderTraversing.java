5c50b9c4adbb894eed680e6ba6099b3b05919035
rithuik <rithuik.jsp@gmail.com>
2020-01-19 22:41:11 +0530

    Commit For Trees

---
+++ src/trees/PreOrderTraversing.java
@@ -0,0 +1,30 @@
+package trees;
+
+import trees.TreeStructure.TreeNode;
+
+public class PreOrderTraversing {
+
+	public static void main(String[] args) 
+	{
+		TreeStructure treeStructure = new TreeStructure();
+		treeStructure.add(55);
+		treeStructure.add(97);
+		treeStructure.add(86);
+		treeStructure.add(103);
+		treeStructure.add(44);
+		treeStructure.add(33);
+		treeStructure.add(46);
+		printPreOrder(treeStructure.getRoot());
+	}
+
+	private static void printPreOrder(TreeNode node) 
+	{
+		if(node == null)
+			return;
+		System.out.print(node.getData()+"; ");
+		printPreOrder(node.getLeft());
+		printPreOrder(node.getRight());
+		
+	}
+
+}
---
+++ src/trees/TreeStructure.java
@@ -0,0 +1,69 @@
+package trees;
+
+public class TreeStructure {
+	private TreeNode root;
+
+	protected class TreeNode 
+	{
+		private Integer data;
+
+		private TreeNode left;
+
+		private TreeNode right;
+
+		public TreeNode(Integer data) {
+			this.data = data;
+		}
+
+		public void setLeft(TreeNode leftNode) {
+			this.left = leftNode;
+		}
+
+		public void setRight(TreeNode rightNode) {
+			this.left = rightNode;
+		}
+
+		public Integer getData() {
+			return this.data;
+		}
+
+		public TreeNode getLeft() {
+			return this.left;
+		}
+
+		public TreeNode getRight() {
+			return this.right;
+		}
+	}
+
+	public void add(Integer data) {
+		if (this.root == null)
+			this.root = new TreeNode(data);
+		else {
+			TreeNode targetNode = this.root;
+			TreeNode temp = null;
+			while (targetNode.left != null && data < targetNode.data) {
+				targetNode = targetNode.left;
+			}
+			while (data > targetNode.data && targetNode.right != null) {
+				targetNode = targetNode.right;
+			}
+
+			if (data < targetNode.data) {
+				temp = targetNode.left;
+				targetNode.left = new TreeNode(data);
+				targetNode.left.left = temp;
+			}
+			if (data > targetNode.data) {
+				temp = targetNode.right;
+				targetNode.right = new TreeNode(data);
+				targetNode.right.right = temp;
+			}
+
+		}
+	}
+
+	public TreeNode getRoot() {
+		return this.root;
+	}
+}
