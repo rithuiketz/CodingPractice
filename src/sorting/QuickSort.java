7a663a5821416df1f338a39f89eb60922d515093
Rithuik Yerrabrolu <rithuik.jsp@gmail.com>
2020-02-02 23:03:49 +0530

    Commit

--- src/sorting/QuickSort.java
+++ src/sorting/QuickSort.java
@@ -2,6 +2,8 @@ package sorting;
 
 import static org.junit.Assert.*;
 
+import java.util.Arrays;
+
 import org.junit.Test;
 
 /**
@@ -12,9 +14,10 @@ public class QuickSort {
 
 	@Test
 	public void test() {
-		int[] testArr = { 64, 25, 12, 22, 11 };
-		int[] expectedArr = { 11, 12, 22, 25, 64 };
+		int[] testArr = { 411,575,686,364,887,183};
+		int[] expectedArr = {183,364,411, 575,686,887};
 		sort(testArr, testArr.length);
+		System.out.println(Arrays.toString(testArr));
 		assertArrayEquals(expectedArr, testArr);
 	}
 
@@ -24,25 +27,31 @@ public class QuickSort {
 	}
 
 	private static void quickSort(int[] testArr, int lowIdx, int highIdx) {
-		if (lowIdx < highIdx) {
+		if (lowIdx < highIdx)
+		{
 			int pivot = partition(testArr, lowIdx, highIdx);
 			quickSort(testArr, lowIdx, pivot - 1);
 			quickSort(testArr, pivot, highIdx);
 		}
 	}
 
-	private static int partition(int[] testArr, int lowIdx, int highIdx) {
-		
-		int pivotIndex  = highIdx;
-		for(int i=lowIdx;i<=highIdx;i++)
-		{
-			if( (i > pivotIndex && testArr[i] <testArr[pivotIndex]) || (i<pivotIndex && testArr[i] > testArr[pivotIndex]))
-			{
-				swap(testArr, i, pivotIndex);
-			    pivotIndex = i;
-			}
-		}
-		return pivotIndex;
+	private static int partition(int[] arr, int low, int high) 
+	{
+		int pivot = arr[high];  
+        int i = (low-1); // index of smaller element 
+        for (int j=low; j<high; j++) 
+        { 
+            // If current element is smaller than the pivot 
+            if (arr[j] < pivot) 
+            { 
+                i++; 
+                swap(arr, i, j);
+            } 
+        } 
+  
+        // swap arr[i+1] and arr[high] (or pivot) 
+        swap(arr, i+1, high);
+         return i+1; 
 	}
 	
 	private static void swap(int[] testArr,int srcIndx,int tgtIndx)
